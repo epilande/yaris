6842bb8c64baab1eea67867c5a2512d690fc0e05
Emmanuel Pilande <manny.pilande@hpe.com>
2016-07-15 01:40:49 -1000

    Switch to browserHistory

--- src/client.js
+++ src/client.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 render(
-  <Router history={hashHistory} routes={routes} />,
+  <Router history={browserHistory} routes={routes} />,
   document.getElementById('app')
 );
